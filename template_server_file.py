4b8d8f91f37007f6640fb8e6505dc361fbe47ad1
Dipesh13 <mangwani.dipesh@gmail.com>
2018-06-26 22:49:40 +0530

    added path to upload folder

--- template_server_file.py
+++ template_server_file.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,flash, redirect, url_for
 from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = './uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+# ALLOWED_EXTENSIONS = set(['csv'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload')
 def upload():
@@ -12,10 +22,27 @@ def upload():
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
+        # if 'file' not in request.files:
+        #     flash('No file part')
+        # if file and allowed_file(file.filename):
+        #     filename = secure_filename(file.filename)
+        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #     return redirect(url_for('uploaded_file',filename=filename))
+
+        file = request.files['file']
+        # file.save(secure_filename(file.filename))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
         return 'file uploaded successfully'
 
+    # return '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <input type=file name=file>
+    #   <input type=submit value=Upload>
+    # </form>
+    # '''
 
 if __name__ == '__main__':
     app.run(debug=True)
