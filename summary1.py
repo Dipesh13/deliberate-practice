7ebffc30dc226cf4aee4be9719621460912fa096
Dipesh13 <mangwani.dipesh@gmail.com>
2018-08-03 03:29:24 +0530

    chapter1 of cs41

---
+++ summary1.py
@@ -0,0 +1,15 @@
+# const and types
+# var
+# num : op
+# bool : truthy falsy
+# str : index, slice , methods
+# conversion bw num, str
+# control flow if elif else
+# ds : 1) list = array/vector . nested and hetrogeneous . split and slice
+#           list methods  : append, len , in ,
+# loops
+# range  : (:stop) or (start,stop) or (start,stop,step)
+# break and continue
+# functions : combinations of args and return (4)
+# args : args have no specific type . keywords args , variadic arg list
+# try different types of args and return : types,ds,fn,objects etc..
