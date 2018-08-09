1ee3e279e4dd89c05b9b0020be4538cf5b94c168
Dipesh13 <mangwani.dipesh@gmail.com>
2018-08-09 13:56:09 +0530

    to do list from cs41

---
+++ tldr.py
@@ -0,0 +1,146 @@
+# one
+# code
+
+# read
+
+
+# two
+# code
+# file io
+## read 1 f.read(size) 2 f.readline() 3 f.readlines()
+## write 1 f.write(string) 2 f.writelines(data) 3 f.flush()
+# executable scripts
+# virtual env : Install virtualenv + virtualenvwrapper
+
+# read
+# duck typing
+
+
+# three
+# code
+# fibonaci example
+# union, intersection and diff for words in files
+# reversed iteration and sorted
+# Comprehensions (as many as possible - list/set/dict)
+
+# read
+# Comprehensions as Higher-Level Transformations
+
+
+# four
+# code
+# try different types of args and return : types,ds,fn,objects etc..
+# try muliple args and return
+# no arg , no return , arg return etc
+# postional args, *args , keyword args , **kwargs
+
+# read
+# First-Class Functions
+# What are function objects?
+# What can you do with function objects?
+# What attributes does a function object possess?
+# Can I pass a function as a parameters to other functions?
+# Can a function return another function?
+# How can I modify a function object?
+
+# five
+# code
+# Lambdas
+# Map Filter Reduce
+# Iterators
+# Generators ; fibonaci example
+# Decorators 1)fn as arg 2)fn as return val 3) fn as both arg and return val
+
+# read
+# function closures
+
+
+# six
+# code
+# code more classes
+# Inheritance : Single and Multiple
+# Magic Methods
+# try except
+# finally
+
+# read
+# Attribute Resolution
+# http://www.diveintopython3.net/special-method-names.html
+# https://docs.python.org/3.4/reference/datamodel.html#specialnames
+
+
+# seven
+# code
+# build a package ; preferably flask web app
+# collections ; namedtuple/Counter/defaultdict
+# itertools : Combinatorics/Infinite Iterators
+# regex
+# json
+# random
+# sys
+# argparse
+# pathlib
+# subprocess and shlex
+# timeit
+# Common One-Liners
+
+# read
+# Executing Modules as Scripts ; __name__ = '__main__'
+# collections
+# itertools
+# subprocess and shlex
+# unicodedata
+# Common One-Liners
+## to-do : google common one-liners python
+## http://book.pythontips.com/en/latest/one_liners.html
+## https://wiki.python.org/moin/Powerful%20Python%20One-Liners
+## https://www.quora.com/What-are-some-of-the-most-elegant-greatest-Python-one-liners
+# other modules
+
+
+# eight
+# code
+# requirements.txt
+# requests package
+# NumPy and Pandas
+# Flask
+
+# read
+# requests package : http://docs.python-requests.org/en/master/user/quickstart/
+# NumPy and Pandas
+# Scipy : https://docs.scipy.org/doc/scipy/reference/index.html
+# PEP8 and AutoPEP8 :# http://pep8.readthedocs.io/en/latest/intro.html # https://github.com/hhatto/autopep8
+# Flask
+# SQLAlchemy
+# Dateutil
+# Basemap
+# robobrowser / mechanize – Automate web tasks.
+# networkx – Visualize small graphs.
+# BeautifulSoup – Rapidly prototype HTML parsers.
+# pygame – Basic game-playing tools.
+# https://docs.python.org/3.4/library/index.html#library-index
+# https://docs.python.org/3.4/reference/index.html
+
+
+# nine
+# code
+
+# read
+
+
+# ten
+# code
+# 21 Common Python Style Tricks
+
+# read
+# https://docs.scipy.org/doc/numpy/user/quickstart.html
+# https://docs.scipy.org/doc/numpy/user/basics.html
+# http://jupyter.org/documentation.html
+# Html / css / js : W3Schools has great tutorials
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+# Foundational Machine Learning Skills:
+    #Take CS229 or CS221! Or just read the course notes
+# In-Depth Machine Learning Tools"
+    # Check out CME 193 and CS 20SI at Stanford!
+    # https://stanford.edu/~schmit/cme193/
+    # http://web.stanford.edu/class/cs20si/
